879a6b9f564c76d384165ddd7e93401823485176
Brian Buchanan <Brian@guest-2-00734.dhcp.unc.edu>
2014-04-20 22:33:51 -0400

    updates

--- tracker/tracker/static_media/js/site.js
+++ tracker/tracker/static_media/js/site.js
@@ -10,3 +10,10 @@ $('.article').readmore({
 		}
 	}
 });
+
+
+//mapbox js
+var map = L.mapbox.map('map', 'examples.map-9ijuk24y').setView([40, -74.50], 9)
+	.featureLayer.setGeoJSON(geojson);
+
+map.scrollWheelZoom.disable();
