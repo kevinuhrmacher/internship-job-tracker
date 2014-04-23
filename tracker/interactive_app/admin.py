6a6a4ba1d9e768353bb9cd4d096ff0cecbbb48c7
Brian Buchanan <bpbuch@gmail.com>
2014-04-22 20:07:52 -0400

    added job posting

--- tracker/interactive_app/admin.py
+++ tracker/interactive_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from interactive_app.models import UserProfile, Organization, City
+from interactive_app.models import UserProfile, Organization, City, JobPosting
 
 admin.site.register(UserProfile)
+admin.site.register(JobPosting)
 
 class OrganizationAdmin(admin.ModelAdmin):
 	search_fields = ('name',)
--- tracker/interactive_app/forms.py
+++ tracker/interactive_app/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from interactive_app.models import UserProfile, City, Organization
+from interactive_app.models import UserProfile, City, Organization, JobPosting
 
 class ProfileForm(ModelForm):
     class Meta:
@@ -18,3 +18,21 @@ class ProfileForm(ModelForm):
           'current_city',
           'current_company'
         ]
+
+class JobForm(forms.Form):
+  JOBTYPE_CHOICES = (
+  ("Internship","Internship"),
+  ("Job","Job")
+  )
+
+  title = forms.CharField(widget=forms.TextInput(), max_length=100)
+  jobType = forms.ChoiceField(choices=JOBTYPE_CHOICES)
+  link = forms.CharField(widget=forms.TextInput(), max_length=100)
+  desc = forms.CharField(widget=forms.Textarea())
+  timeFrame = forms.DateField(widget=forms.DateInput(attrs={'placeholder':'mm/dd/yyy'}))
+  contact = forms.CharField(widget=forms.TextInput(), max_length=100)
+  organization = forms.ModelChoiceField(queryset=Organization.objects.all())
+
+# class JobForm(ModelForm):
+#   class Meta:
+#       model = JobPosting
